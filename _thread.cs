e8c745e35de54246d41220fe306f19fe9cf76bf7
seonginnnnn <rlatjddls7670@naver.com>
2022-11-30 20:16:29 +0900

    server

---
+++ _thread.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        volatile static bool _stop = false; //stop은 휘발성 데이터니까 최적화 금지 //잘 사용 x
+
+        static void ThreadMain()
+        {
+            Console.WriteLine("쓰레드 시작!");
+
+            while (_stop == false)
+            {
+                //누군가가 stop 신호를 해주기를 기다린다.
+            }
+
+            Console.WriteLine("쓰레드 종료!");
+        }
+
+        static void Main(string[] args)
+        {
+            Task t = new Task(ThreadMain);
+            t.Start();
+
+            Thread.Sleep(1000);
+
+            _stop = true;
+
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
+            t.Wait(); //쓰레드 클래스는 t.join()
+
+            Console.Write("종료 성공");
+        }
+    }
+}
