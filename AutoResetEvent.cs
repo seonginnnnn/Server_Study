2a9f168f02b3b84f00f71b8ad2aadce2416e8bf9
seonginnnnn <rlatjddls7670@naver.com>
2022-12-09 19:16:15 +0900

    server

---
+++ AutoResetEvent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Lock
+    {
+        // bool <- 커널
+        AutoResetEvent _avaliable = new AutoResetEvent(true);
+
+        public void Acquire() //획득하겠다
+        {
+            _avaliable.WaitOne(); // 입장 시도
+        }
+
+        public void Release()
+        {
+            _avaliable.Set(); // flag
+        }
+    }
+
+    class Program
+    {
+        static int _num = 0;
+        static Lock _lock = new Lock();
+
+        static void Thread_1()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
+            }
+        }
+
+        static void Thread_2()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(_num);
+        }
+    }
+}
